3d055f238db5d143db8009e8b26048b9c8e0b224
ismailjosim <ismailjosim99@gmail.com>
2026-05-29 23:33:49 +0600

    feat: update Next.js configuration for image optimization and enhance layout component with suppressHydrationWarning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	images: {
+		unoptimized: process.env.NODE_ENV === 'development',
+		remotePatterns: [],
+	},
 }
 
 export default nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 			suppressHydrationWarning
 			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 		>
-			<body className='flex min-h-full flex-col'>
+			<body className='flex min-h-full flex-col' suppressHydrationWarning>
 				<ThemeProvider>
 					{children}
 					<AutoLockModal />
